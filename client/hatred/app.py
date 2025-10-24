86b5b81e0c28a0f53a281a92fbaa1df41442fbdb
Sidharth <sidharthsuresh07@gmail.com>
2025-10-24 19:44:26 -0400

    began working on the app and engine variables

--- client/hatred/app.py
+++ client/hatred/app.py
@@ -1,5 +1,37 @@
 import pygame
+import game_details
 
 class App:
     def __init__(self) -> None:
+        """
+        DO NOT OVERRIDE!
+        """
+
+        self.window = pygame.display.set_mode(game_details.WINDOW_SIZE)
+        
+        pygame.display.set_caption(f"{game_details.GAME_NAME} - {game_details.GAME_VERSION}")
+
+        self.game_running = True
+
+        self.init()
+        self._loop()
+
+    def _loop(self) -> None:
+        """
+        DO NOT OVERRIDE!
+        """
+        while self.game_running:
+            pass
+
+    def init(self) -> None:
+        """
+        This is an overridable function that runs when the game 
+        initialises.
+        """
         pass
+
+    def update(self) -> None:
+        """
+        This is an overridable function that runs when the game updates every 
+        tick.
+        """
--- client/hatred/game_details.py
+++ client/hatred/game_details.py
@@ -1,2 +1,4 @@
 GAME_NAME = "flat adventures"
 GAME_VERSION = "ea1.0.0"
+
+WINDOW_SIZE = (600, 600)
