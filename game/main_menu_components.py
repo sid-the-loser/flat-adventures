0a2c5db71614ea7b7500f2b8ec5cf3f9c061f95d
Sidharth <sidharthsuresh07@gmail.com>
2025-11-19 20:50:30 -0500

    Refactor TitleLabel component to encapsulate title state and improve readability

--- game/main_menu_components.py
+++ game/main_menu_components.py
@@ -8,36 +8,39 @@ BASE_PATH = os.path.dirname(os.path.abspath(__file__))
 
 ASSETS_PATH = os.path.join(BASE_PATH, "assets/")
 
-title_triggered_flag = False
-
 FONTS_PATH = os.path.join(ASSETS_PATH, "fonts/Tiny5-Regular.ttf")
-
-print(FONTS_PATH)
-
-TITLE_FONT = pygame.font.Font(FONTS_PATH, 72)
 
 class TitleLabel(Component):
     def __init__(self, parent_game_object, active: bool = True) -> None:
         super().__init__("TitleLabel", parent_game_object, active)
-        self.img = TITLE_FONT.render("Flat Adventures", True, (255, 255, 255))
-        self.rect = self.img.get_rect()
-        self.rect.x = 50
-        self.rect.y = 250
+        self.title_font = pygame.font.Font(FONTS_PATH, 72)
+
+        self.title_img = self.title_font.render("Flat Adventures", True, 
+                                                (255, 255, 255))
+        self.title_rect = self.title_img.get_rect()
+
+        self.title_rect.x = 50
+        self.title_rect.y = 250
+
+        self.animation_speed_x = self.title_rect.x / self.title_rect.y
+
+        self.title_triggered_flag = False
 
     def update(self):
         global title_triggered_flag
 
         delta = self.game_object.scene.app.delta_time
 
-        if not title_triggered_flag:
+        if not self.title_triggered_flag:
             for event in self.game_object.scene.app.events:
                 if event.type == pygame.KEYDOWN:
-                    title_triggered_flag = True
+                    self.title_triggered_flag = True
 
         else:
-            if self.rect.x != 0 or self.rect.y != 0:
-                self.rect.x = lerp(self.rect.x, 0, delta)
-                self.rect.y = lerp(self.rect.y, 0, delta)
+            if self.title_rect.x != 0 or self.title_rect.y != 0:
+                self.title_rect.x = lerp(self.title_rect.x, 0, 
+                                         delta * self.animation_speed_x)
+                self.title_rect.y = lerp(self.title_rect.y, 0, delta)
 
     def draw(self):
-        self.game_object.scene.app.window.blit(self.img, self.rect)
+        self.game_object.scene.app.window.blit(self.title_img, self.title_rect)
