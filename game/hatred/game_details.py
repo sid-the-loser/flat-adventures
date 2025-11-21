b7437975caea2aaf2ac23e24f22977afb5daadd5
Sidharth Suresh <sidharthsuresh07@gmail.com>
2025-11-20 20:39:54 -0500

    Add FULLSCREEN flag to WINDOW_FLAGS for enhanced display options

--- game/hatred/game_details.py
+++ game/hatred/game_details.py
@@ -4,7 +4,7 @@ GAME_NAME: str = "flat adventures"
 GAME_VERSION: str = "ea0.0.1"
 
 WINDOW_SIZE: tuple = (600, 600)
-WINDOW_FLAGS: int = pygame.RESIZABLE | pygame.SCALED
+WINDOW_FLAGS: int = pygame.RESIZABLE | pygame.SCALED | pygame.FULLSCREEN
 WINDOW_DEFAULT_DISPLAY: int = 0
 WINDOW_VSYNC: int = 0
 WINDOW_TITLE: str = f"{GAME_NAME} - {GAME_VERSION}"
