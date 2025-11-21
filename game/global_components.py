137a11594555828f12014357e73a1527991913c9
Sidharth <sidharthsuresh07@gmail.com>
2025-11-21 15:15:31 -0500

    Enhance debug key logic to manage overlay visibility and fullscreen toggle; add support for LCTRL key state tracking.

--- game/global_components.py
+++ game/global_components.py
@@ -8,11 +8,17 @@ class DebugKeyLogic(GlobalComponent):
         super().__init__("DebugEvents", parent_app)
         self.show_debug_overlay = False
 
+        # NOTE: Mac works weirdly with function keys so, we kinda need this
+        self.debug_alt_key_flag = False 
+
     def early_update(self) -> None:
         events = self.app.events
 
         for event in events:
             if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LCTRL:
+                    self.debug_alt_key_flag = True
+
                 if event.key == pygame.K_F2:
                     pass # TODO: screenshot functionality
 
@@ -26,3 +32,18 @@ class DebugKeyLogic(GlobalComponent):
                 if event.key == pygame.K_ESCAPE:
                     if not IS_BUILD: # TODO: Removed later to support pause menu
                         self.app.switch_to_scene("MainMenu")
+
+                if self.debug_alt_key_flag:
+                    if event.key == pygame.K_p:
+                        pass # TODO: screenshot functionality
+
+                    if event.key == pygame.K_o:
+                        self.show_debug_overlay = not self.show_debug_overlay
+                    
+                    if event.key == pygame.K_l:
+                        pygame.display.toggle_fullscreen()
+                        # NOTE: This function works really badly with some display drivers is you are using pygame 1
+
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LCTRL:
+                    self.debug_alt_key_flag = False
